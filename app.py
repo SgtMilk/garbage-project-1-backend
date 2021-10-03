23d1808269dbc97a39d1ecb2c4a378fb11c49348
SgtMilk <alix.routhier-lalonde@hotmail.com>
2021-10-03 01:33:41 -0400

    better model pt.2

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
+from matplotlib.pyplot import imread
+from ml import predict
 from backend import Backend
 
 if __name__ == "__main__":
+    # picture = imread('./ml/data/source/biological/biological11.jpg')
+    # print(predict(picture))
     app = Backend()
     app.run()
