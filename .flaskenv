fe8a936555261b4bcb6b0f051b2dcf7ca718c60d
SgtMilk <alix.routhier-lalonde@hotmail.com>
2021-10-02 20:07:44 -0400

    cleanup

--- .flaskenv
+++
@@ -1,2 +0,0 @@
-FLASK_APP=backend/backend.py
-FLASK_ENV=development
--- train.py
+++
@@ -1,8 +0,0 @@
-from ml.data import get_images
-from ml import Net
-
-if __name__ == "__main__":
-    dataset = get_images()
-    print("beep")
-    net = Net()
-    net.train(100, dataset, 0.1)
